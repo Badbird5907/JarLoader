71fbd8e33767a3a5d1c401857cee11cb0fdca06e
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-09-29 23:00:29 -0400

    Print how many libs loaded

--- src/main/java/dev/badbird/jarloader/JarLoader.java
+++ src/main/java/dev/badbird/jarloader/JarLoader.java
@@ -8,16 +8,19 @@ public class JarLoader {
     public static void premain(String args, java.lang.instrument.Instrumentation inst) {
         System.out.println("[PREMAIN] Loading Libraries...");
         File libs = new File("lib");
+        int added = 0;
         if (libs.exists()) {
             for (File file : libs.listFiles()) {
                 if (file.getName().endsWith(".jar")) {
                     try {
                         inst.appendToSystemClassLoaderSearch(new JarFile(file));
+                        added++;
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                 }
             }
+            System.out.println("[PREMAIN] Loaded " + added + " libraries.");
         } else {
             System.out.println("[PREMAIN] Libraries folder not found.");
         }
